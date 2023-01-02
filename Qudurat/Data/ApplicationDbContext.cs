31b0d5aedf0fff21b35a28a2b9fec5c690c874da
Hamisco <hamisco27@icloud.com>
2023-01-02 21:13:40 +0200

    Initialization

---
+++ Qudurat/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Qudurat.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityUser>().ToTable("Users", "Security");
+            builder.Entity<IdentityRole>().ToTable("Roes", "Security");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles", "Security");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims", "Security");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins", "Security");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("UserRoleClaim", "Security");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens", "Security");
+
+
+        }
+    }
+}
---
+++ Qudurat/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Qudurat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
